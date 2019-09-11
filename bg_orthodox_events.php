b316ec37a1a7eae9f377e25e843a294dde586077
Vadim <vadim.bogaiskov@gmail.com>
2019-09-11 23:50:02 +0300

    Version 0.2

--- bg_orthodox_events.php
+++ bg_orthodox_events.php
@@ -4,7 +4,7 @@
     Plugin URI: http://bogaiskov.ru/plugin-orthodox-events/
     Description: Плагин выводит на экран события православного календаря: праздники, памятные даты, дни поминовения усопших, дни почитания икон, посты и сплошные седмицы и другую полезную информацию. 
     Author: VBog
-    Version: 0.1
+    Version: 0.2
     Author URI: http://bogaiskov.ru 
 	License:     GPL2
 */
@@ -36,12 +36,21 @@ if ( !defined('ABSPATH') ) {
 	die( 'Sorry, you are not allowed to access this page directly.' ); 
 }
 
-define('BG_ORTODOX_EVENTS_VERSION', '0.1.0');
+define('BG_ORTODOX_EVENTS_VERSION', '0.2.0');
 
 global $bg_orthodox_events;
-// Загрузить файл событий
-$bg_orthodox_events = bg_ortev_load_xml(plugin_dir_path( __FILE__ ).'events.xml');
+include_once('inc/date.php');
 
+// Загрузить файлы событий
+$upload_dir = wp_upload_dir();
+$bg_orthodox_events = bg_ortev_load_xml(plugin_dir_path( __FILE__ ).'calendar.xml');
+$bg_orthodox_events = array_merge($bg_orthodox_events, bg_ortev_load_xml($upload_dir['basedir'].'/bg_ortev/events.xml'));
+//$bg_orthodox_events = bg_ortev_load_xml($upload_dir['basedir'].'/bg_ortev/events.xml');
+
+/*******************************************************************************
+	Функция загружает XML-файл и преобразует его в массив
+	XML-файл должен содержать не менее 2-х записей
+*******************************************************************************/  
 function bg_ortev_load_xml($path) {
 	
 	$xmlfile = file_get_contents($path); 	// Read entire file into string 
@@ -54,133 +63,101 @@ function bg_ortev_load_xml($path) {
   
 add_shortcode( 'ortev_dayinfo', 'bg_ortev_dayinfo_shortcode' );
 /*******************************************************************************
-// Функция обработки шорт-кода ortev_dayinfo
+	Функция обработки шорт-кода ortev_dayinfo
 *******************************************************************************/  
 function bg_ortev_dayinfo_shortcode($atts) {
 	extract( shortcode_atts( array(
 		'date' => '',
-		'type' => ''
+		'type' => '',
+		'separator'=> ''
 	), $atts ) );
 	
 	global $bg_orthodox_events;
 	
-	if (empty($date)) {
-		if (!empty($_GET['date'])) $date = $_GET['date'];
-		else $date = date('Y-m-d');
+	if (empty($date)) {	// Дата не задана
+		if (!empty($_GET['date'])) $date = $_GET['date'];	// Ищем дату в адресной строке
+		else $date = date('Y-m-d');							// Иначе "сегодня"
 	}
+	$date = bg_ortev_oldDate($date);			// Дата по старому стилю
+	list($y, $m, $d) = explode('-', $date);		// Год, месяц, день
+	$easter = bg_ortev_easter($y);				// Пасха по старому стилю
+	$date = bg_ortev_date ($y, $m, $d);
+	
+	// Массив разрешенных для отображения типов событий
+	if ($type) $typeArr = explode(',', $type);
 
 	$quote = '';
-	$date = bg_ortev_oldDate($date);			// Дата по старому стилю
-	list($y, $m, $d) = explode('-', $date);
-	$easter = bg_ortev_easter($y);				// Пасха по старому стилю
 
-//	$quote .= $date.'<br>';
-	
+	// Для каждого события в БД
 	foreach ($bg_orthodox_events as $event) {
-		if ($event['f_month'] == 0) {
-			$finish_date = bg_ortev_shiftDate($easter, $event['f_date']);
-		} else {
-			$finish_date = $y.'-'.$event['f_month'].'-'.$event['f_date'];
-		}
-		if ($event['s_month'] == 0) {
-			$start_date = bg_ortev_shiftDate($easter, $event['s_date']);
-		} else {
-			$start_date = $y.'-'.$event['s_month'].'-'.$event['s_date'];
-		}
-		if (!empty($event['name']) && ($start_date <= $date && $finish_date >= $date)) {
-			if (!empty($event['link'])) {
-				$quote .= ' <a href="'.$event['link'].'" title="'.$event['discription'].'">'.$event['name'].'</a><br>';
-			} else {
-				$quote .= $event['name'].'<br>';
+		// Проверяем разрешенные типы событий
+		if ($type && !in_array($event['type'], $typeArr)) continue;
+		
+		/* Дата окончания события */
+		// Подвижные события относительно Пасхи
+		if ($event['f_month'] == 0) $finish_date = bg_ortev_shiftDate($easter, $event['f_date']);
+		// Неподвижные события
+		elseif ($event['f_month'] > 0 && $event['f_month'] < 13) $finish_date = bg_ortev_date ($y, $event['f_month'], $event['f_date']);
+		// Другие случаи нерелевантны - ошибка
+		else continue;
+
+		/* Дата начала события */
+		// Особые случаи подвижных событий
+		if ($event['s_month'] > -5 && $event['s_month'] < 0) {			
+			$finish_wd = bg_ortev_WeekDay ($finish_date);
+			switch ($event['s_month']) {
+			case -1:		// --- Сб./Вс. перед/после события
+				$dd = $event['s_date'] - $finish_wd;					// смещение относительно даты события
+				if (!$dd) $event['name'] = '';							// в день основного события не отмечается
+				$finish_date = bg_ortev_shiftDate($finish_date, $dd);			
+				break;
+			case -2:		// --- Событие в Сб./Вс. перед/после даты
+				$dd = $event['s_date'] - $finish_wd;					// смещение относительно указанной даты
+				$finish_date = bg_ortev_shiftDate($finish_date, $dd);
+				break;
+			case -3:		// --- Событие в только указанный день недели
+				if (!($finish_wd == $event['s_date'])) $event['name'] = '';
+				break;
+			case -4:		// --- Событие в ближайшее Сб./Вс. к дате
+				$dd = $event['s_date'] - $finish_wd;					// смещение относительно указанной даты
+				$dd = $dd + ($finish_wd>3?7:0);							// отсчет от середины недели
+				$finish_date = bg_ortev_shiftDate($finish_date, $dd);
+				break;
 			}
+			$start_date = $finish_date;
+		}
+		// Подвижные события относительно Пасхи
+		elseif ($event['s_month'] == 0) $start_date = bg_ortev_shiftDate($easter, $event['s_date']);
+		// Неподвижные события
+		elseif ($event['s_month'] > 0 && $event['s_month'] < 13) $start_date = bg_ortev_date ($y, $event['s_month'], $event['s_date']);
+		// Другие случаи нерелевантны - ошибка
+		else continue;
+		
+		// Обрабатываем случай, когда событие начинается в одном году, а заканчивается в следующем
+		if ($start_date > $finish_date) {
+			if ($start_date <= $date) {	//	Если текущая дата ПОЗЖЕ даты начала события,
+										//	то дата окончания события в следующем году
+				if ($event['f_month'] == 0) $finish_date = bg_ortev_shiftDate(bg_ortev_easter($y+1), $event['f_date']);
+				else $finish_date = bg_ortev_date ($y+1, $event['f_month'], $event['f_date']);
+			} else { 					// 	Если текущая дата ДО даты начала события, 
+										// 	то дата начала события в предыдущем году
+				if ($event['s_month'] == 0) $start_date = bg_ortev_shiftDate(bg_ortev_easter($y-1), $event['s_date']);
+				else $start_date = bg_ortev_date ($y-1, $event['s_month'], $event['s_date']);
+			}
+		}
+		
+		// Проверяем попадает ли заданная дата в диапазон дат события
+		if (!empty($event['name']) && ($start_date <= $date && $finish_date >= $date)) {
+//			$quote .= $start_date." ". $date." ". bg_ortev_WeekDayName (bg_ortev_WeekDay ($date))." ". $finish_date."<br>";
+			if (empty($event['link'])) $q = $event['name'];
+			else $q = '<a href="'.$event['link'].'" title="'.$event['discription'].'">'.$event['name'].'</a>';
+			$q = '<span class="bg_ortev_type'.$event['type'].'">'.$q.'</span>';
+			if ($separator == 'ul' || $separator == 'ol') $quote .= '<li>'.$q.'</li>';
+			else $quote .= $q.$separator;
 		}	
 	}
+	if ($quote && $separator == 'ul') $quote = '<ul>'.$quote.'</ul>';
+	elseif ($quote && $separator == 'ol') $quote = '<ol>'.$quote.'</ol>';
 	return "{$quote}";
 }
 
-/*******************************************************************************
-// Функция смещает дату по старому стилю на заданное количество дней
-*******************************************************************************/  
-function bg_ortev_shiftDate($date, $dd) {
-	list($y, $m, $d) = explode('-', $date);
-	
-	if ($dd < 0) {
-		while ($d + $dd < 1) {
-			$m--;
-			$dd = bg_ortev_numDays($m-1, $y)+ $dd;
-			if ($m < 1) {
-				$m = 12;
-				$y--;
-			}
-		}
-	} elseif ($dd > 0) {
-		while ($d + $dd > bg_ortev_numDays($m-1, $y)) {
-			$m++;
-			$dd = $dd - bg_ortev_numDays($m-1, $y);
-			if ($m > 12) {
-				$m = 1;
-				$y++;
-			}
-		}
-	}
-	$d = $d + $dd;
-	return $y.'-'.$m.'-'.$d;
-}
-
-
-/*******************************************************************************
-// Функция преобразует дату по новому стилю в дату по старому стилю
-*******************************************************************************/  
-function bg_ortev_oldDate($date) {
-	list($y, $m, $d) = explode('-', $date);
-	
-	$dd = bg_ortev_dd($y);
-	if ($d - $dd < 1) {
-		$m--;
-		if ($m < 1) {
-			$m = 12;
-			$y--;
-		}
-		$d = bg_ortev_numDays($m-1, ($m<3)?($y-1):$y) + $d - $dd;
-	}
-	return $y.'-'.$m.'-'.$d;
-}
-
-/*******************************************************************************
-// Функция возвращает количество дней в месяце
-*******************************************************************************/  
-function bg_ortev_numDays ($month, $year) {
-	$dim = array(31,28,31,30,31,30,31,31,30,31,30,31);
-	return ($month == 2 && bg_ortev_isLeap($year)) ? 29 : $dim[(int)$month-1];
-}
-
-
-/*******************************************************************************
-// Функция возвращает количество дней между датами по новому и старому стилю
-*******************************************************************************/  
-function bg_ortev_dd($year) {
-	return ($year-$year%100)/100 - ($year-$year%400)/400 - 2;
-}
-/*******************************************************************************
-// Функция определяет день Пасхи на заданный год по старому стилю
-*******************************************************************************/
-
-function bg_ortev_easter($year) {
-	$a=((19*($year%19)+15)%30);
-	$b=((2*($year%4)+4*($year%7)+6*$a+6)%7);
-	if ($a+$b>9) {
-		$day=$a+$b-9;
-		$month=4;
-	} else {
-		$day=22+$a+$b;
-		$month=3;
-	}
-	return $year.'-'.$month.'-'.$day;
-}
-
-/*******************************************************************************
-// Функция определяет является ли указанный год високосным
-*******************************************************************************/  
-function bg_ortev_isLeap($year) {
-	return (!($year % 4) && ($year % 100) || !($year % 400));
-}
